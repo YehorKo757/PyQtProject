d9f4ec51b4d1b84ef3098c9b2d69503e95e47da0
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2025-01-06 14:30:31 +0000

    Checkable button and report of the checked status

--- basic/signals_and_slots.py
+++ basic/signals_and_slots.py
@@ -13,12 +13,17 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My App")
 
         button = QPushButton("Press Me!")
+        button.setCheckable(True)
         button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
 
         self.setCentralWidget(button)
 
     def the_button_was_clicked(self):
         print("Clicked!")
+
+    def the_button_was_toggled(self, is_checked):
+        print("Clicked?", is_checked)
 
 
 app = QApplication(sys.argv)
