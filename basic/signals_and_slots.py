751d69d1ff11dff27306742fe4d06135991dea8e
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2025-01-06 15:03:25 +0000

    Slots and Signals: connect custom slot to a Qt QMainWindow signal

--- basic/signals_and_slots.py
+++ basic/signals_and_slots.py
@@ -1,4 +1,5 @@
 import sys
+from random import choice
 
 from PyQt6.QtWidgets import (
     QApplication,
@@ -6,20 +7,42 @@ from PyQt6.QtWidgets import (
     QPushButton,
 )
 
+window_titles = [
+    "My App",
+    "Still My App",
+    "Test App",
+    "What on earth",
+    "This is surprising",
+    "Something went wrong",
+]
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.n_times_clicked = 0
 
         self.setWindowTitle("My App")
 
         self.button = QPushButton("Press Me!")
         self.button.clicked.connect(self.the_button_was_clicked)
 
+        self.windowTitleChanged.connect(self.the_window_title_changed)
+
         self.setCentralWidget(self.button)
 
     def the_button_was_clicked(self):
-        self.button.setText("You already clicked me.")
-        self.button.setEnabled(False)
+        print("clicked.")
+        new_window_title = choice(window_titles)
+        print(f"Setting title: {new_window_title}")
+        self.setWindowTitle(new_window_title)
+
+    def the_window_title_changed(self, window_title):
+        print(f"Window title changed: {window_title}")
+
+        if window_title == "Something went wrong":
+            self.button.setDisabled(True)
 
 
 app = QApplication(sys.argv)
