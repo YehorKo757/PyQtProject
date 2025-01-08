7cd87bed46d9466bd4dfb909c4dd000d4dddc959
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2025-01-08 15:53:03 +0000

    QSpinBox test

---
+++ basic/widgets_QSpinBox.py
@@ -0,0 +1,45 @@
+import sys
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QSpinBox,
+    QMainWindow,
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        widget = QSpinBox()
+
+        widget.setMinimum(-10)
+        widget.setMaximum(3)
+        # Or: widget.setRange(-10, 3)
+
+        widget.setPrefix("$")
+        widget.setSuffix("c")
+        widget.setSingleStep(3)  # Or: 0.5 for QDoubleSpinBox
+        widget.valueChanged.connect(self.value_changed)
+        widget.textChanged.connect(self.value_changed_str)
+
+        self.setCentralWidget(widget)
+
+
+    def value_changed(self, i):
+        print(i)
+
+
+    def value_changed_str(self, s):
+        print(s)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
